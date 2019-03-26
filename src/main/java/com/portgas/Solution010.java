fb0526d43f69226739e0cd410b0fa17dd1136934
portgas.ly <portgas.ly@foxmail.com>
2019-03-27 00:27:08 +0800

    变态青蛙

--- src/main/java/com/portgas/Solution010.java
+++ src/main/java/com/portgas/Solution010.java
@@ -60,7 +60,7 @@ public class Solution010 {
     // target = 3   3
     // target = 4   5
     // target = 5   8
-    public int JumpFloor(int target) {
+    public int jumpFloor(int target) {
         if (target <= 2) {
             return target;
         }
@@ -73,6 +73,20 @@ public class Solution010 {
             b = result;
         }
         return result;
+    }
+
+    /**
+     * 变态青蛙：
+     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
+     * 求该青蛙跳上一个n级的台阶总共有多少种跳法。
+     **/
+
+    // f(n) = f(n-1) + f(n-2) +f(n-3) ... + f(1)
+    // f(n-1) = f(n-2) + f(n-3) + ... + f(0)
+    // 所以 f(n) - f(n-1) = f(n-1)
+    // f(n) = 2*f(n-1)
+    public int jumpFloorII(int target) {
+        return (int) Math.pow(2, target - 1);
     }
 
 }
