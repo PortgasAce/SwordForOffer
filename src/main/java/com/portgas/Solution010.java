28547e420e4d49c3694f8bfb513cc16854074138
portgas.ly <portgas.ly@foxmail.com>
2019-03-26 23:53:04 +0800

    跳台阶

--- src/main/java/com/portgas/Solution010.java
+++ src/main/java/com/portgas/Solution010.java
@@ -50,5 +50,29 @@ public class Solution010 {
         return result;
     }
 
+    /**
+     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。
+     * 求该青蛙跳上一个n级的台阶总共有多少种跳法
+     * （先后次序不同算不同的结果）。
+     **/
+    // target = 1   1
+    // target = 2   2
+    // target = 3   3
+    // target = 4   5
+    // target = 5   8
+    public int JumpFloor(int target) {
+        if (target <= 2) {
+            return target;
+        }
+        int a = 1;
+        int b = 2;
+        int result = 0;
+        for (int i = 3; i <= target; i++) {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+        return result;
+    }
 
 }
